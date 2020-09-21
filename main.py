f2de21126f92ac16cbe9d1fbcb14dcbef93ae46d
nileshhazra <nileshhz99@gmail.com>
2020-09-21 15:20:44 +0530

    refactor code

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 import time
 
+# Variables
+path = 'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
+web_path = 'https://docs.google.com/forms/d/e/1FAIpQLSeJVgNegjlq0IkY79KoxfY6ThtnFMI1ykZGw8thGLmu58IOtw/viewform?usp=sf_link'
+name_field = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input'
+school_field = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input'
+number_field = '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input'
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
+# Initialising webdriver for chrome
+driver = webdriver.Chrome(path)
 
-driver.get('https://docs.google.com/forms/d/e/1FAIpQLSeJVgNegjlq0IkY79KoxfY6ThtnFMI1ykZGw8thGLmu58IOtw/viewform?usp=sf_link')
-# driver.implicitly_wait(50)
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-# driver.implicitly_wait(50)
-# print("Element is visible? " + str(py_input.is_displayed()))
+# getting the URL
+driver.get(web_path)
+
+py_input = driver.find_element_by_xpath(name_field)
 time.sleep(0.1)
-py_input.send_keys('Nilesh Hazra')
-# driver.implicitly_wait(20)
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-py_input.send_keys('Birla Institute of technology , Mesra')
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-py_input.send_keys('7488172988')
+py_input.send_keys('Laura Palmer')
+
+py_input = driver.find_element_by_xpath(school_field)
+py_input.send_keys('Georgia Institute of technology')
+
+py_input = driver.find_element_by_xpath(number_field)
+py_input.send_keys('7488171988')
 
 py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[2]').click()
-driver.implicitly_wait(10)
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[4]').click()
+time.sleep(0.1)
+driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[4]').click()
 
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
-driver.implicitly_wait(10)
-py_input = driver.find_element_by_xpath('//*[@id="i5"]/div[3]/div').click()
+time.sleep(0.1)
+driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
 
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span').click()
+driver.find_element_by_xpath('//*[@id="i5"]/div[3]/div').click()
 
-py_input = driver.find_element_by_xpath('//*[@id="i8"]/div[3]/div').click()
-py_input = driver.find_element_by_xpath('//*[@id="i30"]/div[3]/div').click()
+driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span').click()
 
-py_input = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span').click()
+driver.find_element_by_xpath('//*[@id="i8"]/div[3]/div').click()
 
-driver.close()
+driver.find_element_by_xpath('//*[@id="i30"]/div[3]/div').click()
 
+driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div[2]/span').click()
 
+# driver.close()
 
-
-
-
+# driver.implicitly_wait(50)
+# print("Element is visible? " + str(py_input.is_displayed()))
 # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[4]/span -- open options --running not selecting
 # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[4]/span --selected option --not running
 # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[2]/div[4] --div for option --not running
 # //*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[1]/div[4] --div for selected option --not running
-
-
